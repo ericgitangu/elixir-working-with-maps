cb0a1a54d16d43714afd9c8eaebdcbfa0ae4a5f9
Eric <developer.ericgitangu@gmail.com>
2024-02-17 15:25:24 -0600

    Elixir - working with maps, lists with maps added, doctests added

--- lib/maps.ex
+++ lib/maps.ex
@@ -1,6 +1,6 @@
 defmodule Maps do
   @moduledoc """
-  Elixir module for working with maps.
+  Elixir module for working with maps and lists.
   """
 
   @doc """
@@ -8,11 +8,11 @@ defmodule Maps do
 
   ## Examples
 
-      iex> Maps.map_colors()
-      %{primary: "red", secondary: "green", tertiary: "blue"}
+  iex> Maps.map_colors()
+  %{primary: "red", secondary: "green", tertiary: "blue"}
   """
   @spec map_colors() :: map
-  def map_colors do
+  def map_colors() do
     %{primary: "red", secondary: "green", tertiary: "blue"}
   end
 
@@ -21,12 +21,12 @@ defmodule Maps do
 
   ## Examples
 
-      iex> Maps.update_color(%{primary: "red", secondary: "green", tertiary: "blue"}, :primary, "maroon")
-      %{primary: "maroon", secondary: "green", tertiary: "blue"}
+  iex> Maps.update_color(%{primary: "red", secondary: "green", tertiary: "blue"}, :primary, "maroon")
+  %{primary: "maroon", secondary: "green", tertiary: "blue"}
   """
   @spec update_color(map(), atom, any) :: map()
   def update_color(map, key, value) do
-    Map.put(map, key, value)
+    %{map | key => value}
   end
 
   @doc """
@@ -34,13 +34,54 @@ defmodule Maps do
 
   ## Examples
 
-      iex> Maps.run_pipeline()
-      %{primary: "maroon", secondary: "green", tertiary: "blue"}
+  iex> Maps.run_pipeline()
+  %{primary: "maroon", secondary: "green", tertiary: "blue"}
   """
   @spec run_pipeline() :: map
   def run_pipeline do
     map = map_colors()
-    updated_map = update_color(map, :primary, "maroon")
-    updated_map
+    update_color(map, :primary, "maroon")
+  end
+
+  @doc """
+  Converts a map to a list.
+
+  ## Examples
+
+  iex> Maps.list_map_color()
+  [{:primary, "red"}, {:secondary, "green"}, {:tertiary, "blue"}]
+  """
+  @spec list_map_color() :: [{atom, any}]
+  def list_map_color do
+    map_colors()
+    |> Map.to_list()
+  end
+
+  @doc """
+  Converts a list to a map.
+
+  ## Examples
+
+  iex> Maps.map_list_color()
+  %{primary: "red", secondary: "green", tertiary: "blue"}
+  """
+  @spec map_list_color() :: map
+def map_list_color do
+    list_map_color()
+    |> Enum.into(%{})
+  end
+
+  @doc """
+  Retrieves a value from a map.
+
+  ## Examples
+
+  iex> Maps.get_color_from_map()
+  "red"
+  """
+  @spec get_color_from_map() :: any
+  def get_color_from_map() do
+    map_colors()
+    |> Map.get(:primary)
   end
 end
