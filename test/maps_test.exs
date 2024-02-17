cb1db06b3678c8bfe4d72e23be2503e2d080489d
Eric <developer.ericgitangu@gmail.com>
2024-02-17 15:27:13 -0600

    Elixir - working with maps, lists with maps added, casetests added

--- test/maps_test.exs
+++ test/maps_test.exs
@@ -9,4 +9,17 @@ defmodule MapsTest do
   test "update_color/3 updates the value of a key in the map" do
     assert Maps.update_color(%{primary: "red", secondary: "green", tertiary: "blue"}, :primary, "maroon") == %{primary: "maroon", secondary: "green", tertiary: "blue"}
   end
+
+  test "run_pipeline/0 runs a series of map functions" do
+    assert Maps.run_pipeline() == %{primary: "maroon", secondary: "green", tertiary: "blue"}
+  end
+
+  test "list_map_color/0 converts a map to a list" do
+    assert Maps.list_map_color() == [{:primary, "red"}, {:secondary, "green"}, {:tertiary, "blue"}]
+  end
+
+  test "map_list_color/0 converts a list to a map" do
+    assert Maps.map_list_color() == %{primary: "red", secondary: "green", tertiary: "blue"}
+  end
+
 end
